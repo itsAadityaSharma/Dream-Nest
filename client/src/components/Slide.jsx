cf141fd2c6e2538d00e88ae26a3b0b18468571eb
Aaditya Sharma <saaditya940@gmail.com>
2024-05-05 16:16:49 +0530

    SliderbarIcon

--- client/src/components/Slide.jsx
+++ client/src/components/Slide.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import "../styles/Slide.scss";
 
-const Slice = () => {
-  return <div></div>;
+const Slide = () => {
+  return (
+    <div className="slide">
+      <h1>
+        Welcome Home! Anywhere you roam <br />
+        Stay in the moment. Make your memories
+      </h1>
+    </div>
+  );
 };
 
-export default Slice;
+export default Slide;
